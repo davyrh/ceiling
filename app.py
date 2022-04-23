2be35f4cce38fa7158c6cc63f163643e12483638
davyrh <104127167+davyrh@users.noreply.github.com>
2022-04-23 09:25:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,21 +4,21 @@ from PIL import Image, ImageOps
 import numpy as np
 import streamlit as st
 import tensorflow as tf
-st.title("Identification the Condition of Ceilling of the House")
+st.title("Identification the Condition of Ceiling of the House")
 st.header("Architecture Image Detection")
-st.text("Upload an image of any piece in  Ceilling's house for Identification ")
+st.text("Upload an image of any piece in  Ceiling's house for Identification ")
 from img_classification import teachable_machine_classification            
-uploaded_file = st.file_uploader("Choose an image of wall  in the house ...", type="jpg")
+uploaded_file = st.file_uploader("Choose an image of ceiling in the house ...", type="jpg")
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption='Uploaded Image.', use_column_width=True)
     st.write("")
     st.write("Identifying the ceilling...")
-    label = teachable_machine_classification(image, 'keras_model_ceilling.h5')
+    label = teachable_machine_classification(image, 'keras_model_ceiling.h5')
     if label == 0:
-        st.write(" The ceilling is good ")
+        st.write(" The ceiling is good ")
                           
     else:
-        st.write("The ceilling is bad")
+        st.write("The ceiling is bad")
         
            
